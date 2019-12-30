da50e52781fe05e994924a784c457e45ed42542a
gyadav <Nyucox24>
2019-12-30 11:41:19 +0530

    init commit

---
+++ src/main/java/com/javasampleapproach/rabbitmq/SpringRabbitMqConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.javasampleapproach.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitMqConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRabbitMqConsumerApplication.class, args);
+	}
+}
